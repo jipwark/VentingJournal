4c385672b2539befb589109ae7bef10d2dcd5f1b
Medha Kappagantu <79764472+jipwark@users.noreply.github.com>
2022-08-02 16:33:29 -0700

    initial

---
+++ VentingJournal/VentingJournal.swift
@@ -0,0 +1,13 @@
+//
+//  VentingJournal.swift
+//  VentingJournal
+//
+//  Created by scholar on 8/1/22.
+//
+
+import UIKit
+
+class VentingJournal{
+    var ventName = ""
+    var ventText = ""
+}
